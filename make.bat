218451d7c63e2bdfba1ffadc8a683d5606b08d58
Ilya <nuarcane@yandex.ru>
2022-10-31 23:56:39 +0300

    Checkpoint

---
+++ make.bat
@@ -0,0 +1,3 @@
+PATH=C:\Users\ivano\Documents\Univer\cross\mingw64\bin
+gcc -std=c99 -Wall -Wpedantic pract1.c
+a.exe
--- pract/make.bat
+++
@@ -1,3 +0,0 @@
-PATH=C:\Users\Student\Downloads\pract1\mingw64\bin
-gcc -std=c99 -Wall -Wpedantic task2.c
-a.exe
