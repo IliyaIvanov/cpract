750110046fc73ff3fbf4418d8f80e154df34457b
Ilya <nuarcane@yandex.ru>
2022-12-12 21:14:37 +0300

    Checkpoint v2.0

--- make.bat
+++ make.bat
@@ -1,3 +1,11 @@
-PATH=C:\Users\ivano\Documents\Univer\cross\mingw64\bin
-gcc -std=c99 -Wall -Wpedantic project.c
-a.exe
+MyProject : project.o project.c
+	gcc project.o -o project
+	./a.exe
+MyProject : project.c
+	gcc -c project.c -o project.o
+installemsdk: 
+	cd .. && git clone https://github.com/emscripten-core/emsdk.git
+	cd ../emsdk && emsdk install latest && emsdk activate latest && emsdk_env.bat && emcc project.c -s WASM=1 -o pge.html
+	npm i http-server -g
+runserver:
+	cd ../emrun pge.html
--- project.c
+++ project.c
@@ -4,30 +4,6 @@
 #include <math.h>
 #include <stdlib.h>
 
-/*struct xorshift32_state* global_state = NULL;
-
-struct xorshift32_state {
-    uint32_t a;
-};
-
-uint32_t xorshift32(struct xorshift32_state* state) {
-    uint32_t x = state->a;
-    x ^= x << 13;
-    x ^= x >> 17;
-    x ^= x << 5;
-    return state->a = x;
-}
-
-float rnd(float upper) {
-    if (global_state == NULL) {
-        int size = sizeof(struct xorshift32_state);
-        struct xorshift32_state* pointer = malloc(size);
-        global_state = pointer;
-    }
-    global_state->a = xorshift32(global_state);
-    return (global_state->a / (float)UINT32_MAX) * upper;
-}*/
-
 #define MIN(X,Y) (((X) < (Y)) ? (X) : (Y))
 
 float state = 0.1;
